a97b389ee66c2b3da7e5529ad55534050e8b434f
Joe Martin <joe@desertflood.com>
2016-02-29 16:40:06 -0600

    Stop ignoring my customizations

---
+++ custom/aliases.zsh
@@ -0,0 +1,4 @@
+# Define shortcuts'n'at
+
+alias econv='/Applications/calibre.app/Contents/MacOS/ebook-convert'
+alias fixbrew='sudo chown $(whoami):admin /usr/local && sudo chown -R $(whoami):admin /usr/local'
