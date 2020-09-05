900d6d66f220b1eb31b9c99244598bc0a4cd7802
Joe Martin <joe@desertflood.com>
2020-09-05 12:50:14 -0700

    Add in local config for underworld

---
+++ custom/locals/underworld-jmartin.zsh
@@ -0,0 +1,5 @@
+export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3
+export WORKON_HOME=$HOME/.virtualenv
+export PROJECT_HOME=$HOME/development
+export VIRTUALENVWRAPPER_VIRTUALENV=/usr/bin/virtualenv
+source /usr/share/virtualenvwrapper/virtualenvwrapper.sh
